e1022108375919e5c066728450839bbe3718987d
Axell Brendow Batista Moreira <breno.axel@gmail.com>
2020-07-21 14:29:34 -0300

    Create the necessary instructions to run the project without problems (#3)
    * Add script to create a .env for testing
    
    * Remove unnecessary COPY command
    
    This COPY is not necessary because in docker-compose.yml we are already
    doing the volume mapping
    
    * Fix somethings in entrypoint.sh
    
    Now the script will try to migrate until database is ready for
    connection. Also, I added the key:generate because .env.example does not
    has a key
    
    * Add instructions on how to run the project
    
    * Add requirements to run the project
    
    * Fix entrypoint.sh
    
    Install composer dependencies before run php artisan

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,19 @@
 
 composer install
 
-php artisan migrate
+php artisan key:generate
+
+function tryToMigrate() {
+    timeout=5
+    php artisan migrate
+
+    if [ $? -ne 0 ]; then
+        echo "Migration failed. Trying again in $timeout seconds..."
+        sleep $timeout
+        tryToMigrate
+    fi
+}
+
+tryToMigrate
 
 php artisan serve --host=0.0.0.0
