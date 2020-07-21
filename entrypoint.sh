0351a4104c1e988e39132855a6607f25fd9d9b81
axell-brendow <breno.axel@gmail.com>
2020-07-21 10:17:08 -0300

    Fix somethings in entrypoint.sh
    Now the script will try to migrate until database is ready for
    connection. Also, I added the key:generate because .env.example does not
    has a key

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env sh
 
+php artisan key:generate
+
 composer install
 
-php artisan migrate
+function tryToMigrate() {
+    timeout=5
+    php artisan migrate
+
+    if [ $? -ne 0 ]; then
+        echo "Migration failed. Trying again in $timeout seconds..."
+        sleep $timeout
+        tryToMigrate
+    fi
+}
+
+tryToMigrate
 
 php artisan serve --host=0.0.0.0
