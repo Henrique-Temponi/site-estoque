36b71238c57a65b74917e67e6f763ccd614906f9
henrique-temponi <tanzenheavely@gmail.com>
2020-07-26 20:18:42 -0300

    Melhora um pouco home do usuario

--- resources/views/web/usuario/index.blade.php
+++ resources/views/web/usuario/index.blade.php
@@ -10,9 +10,9 @@
         <div class="col s12 m6">
             <div class="card blue-grey darken-1">
                 <div class="card-content white-text">
-                <span class="card-title">{{ $p->voo }}</span>
-                    <p>{{ $p->destino->nome }}</p>
-                    <p>{{ $p->compahia->nome }}</p>
+                <span class="card-title">Nome do voo:{{ $p->voo }}</span>
+                    <p>Nome do destino: {{ $p->destino->nome }}</p>
+                    <p>Nome da compahia: {{ $p->compahia->nome }}</p>
                 </div>
                 <div class="card-action">
                     <a href="{{ route('usuario.reservar.deletar', $p->id) }}">Deletar Reserva</a>
