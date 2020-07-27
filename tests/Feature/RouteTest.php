7c7ee1a42e58d3e0fc2ab386c70bbc637f8beec8
henrique-temponi <tanzenheavely@gmail.com>
2020-07-27 12:19:52 -0300

    Adiciona testes

---
+++ tests/Feature/RouteTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RouteTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+
+    /**
+     * A basic feature test example.
+     *
+     * @test 
+     * @return void
+     */
+    public function User_routes()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        $response = $this->get('/registrar');
+        $response->assertStatus(200);
+
+        
+    }
+
+    /**
+     * @test
+     */
+    public function Protected_routes()
+    {
+        $response = $this->get('/logout');
+        $response->assertStatus(302);
+
+        $response = $this->get('/usuario/reservar/1');
+        $response->assertStatus(302);
+    }
+
+    /**
+     * @test
+     */
+    public function Authtoziation_routes()
+    {
+        $response = $this->get('/admin');
+        $response->assertStatus(302);
+    }
+
+    /** @test */
+    public function Test_with_assertRedirect()
+    {
+        $response = $this->get('/admin')
+            ->assertRedirect('/login');
+    }
+
+    // /** @test */
+    // public function Test_with_actingAs()
+    // {
+    //     $this->actingAs(factory(User::class)->create());
+
+    //     // dd(factory(User::class)->create());
+
+    //     $response = $this->get('/usuario/voos')
+    //         ->assertOk();
+    // }
+
+    
+}
