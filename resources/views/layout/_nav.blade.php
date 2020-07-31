f755a97b6d775fba6bc2f67e2590f101e6e32477
henrique-temponi <tanzenheavely@gmail.com>
2020-07-31 09:40:18 -0300

    Melhora navegacao home e login

--- resources/views/layout/_nav.blade.php
+++ resources/views/layout/_nav.blade.php
@@ -1,17 +1,50 @@
 <nav class="navbar navbar-expand navbar-primary navbar-dark">
     <!-- Left navbar links -->
-    <ul class="navbar-nav">
-    <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-    </li>
-    <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Home</a>
-    </li>
-    <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-    </li>
-    </ul>
+    <div class="container">
+        <ul class="navbar-nav">
+        <!-- <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+        </li> -->
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="{{ route('site.home') }}" class="nav-link">Home</a>
+        </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+      <!-- Messages Dropdown Menu -->
+      @if(Auth::guest())
+        <li class="nav-item d-none d-sm-inline-block">
+            <a class="nav-link" href="{{ route('site.login') }}">Fazer login</a>
+        </li>
 
+        <li class="nav-item d-none d-sm-inline-block">
+            <a class="nav-link" href="{{ route('site.registrar') }}">Registrar-se</a>
+        </li>
+      @else
+        <li class="nav-item d-none d-sm-inline-block">
+            <a class="nav-link" href="#">Bem vindo, {{Auth::user()->name}}</a>
+        </li>
+        @if(Auth::user()->admin)
+            <li class="nav-item d-none d-sm-inline-block">
+                <a class="nav-link" href="{{ route('admin.index') }}">Painel admin</a>
+            </li>
+        @endif
+        <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#">
+            <i class="fas fa-bars"></i>
+            <!-- <span class="badge badge-danger navbar-badge">3</span> -->
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <a href="{{ route('usuario.voos') }}" class="dropdown-item">
+                    <i class="fas fa-plane"></i> Meus voos
+                </a>
+                <a href="{{ route('site.logout') }}" class="dropdown-item">
+                    <i class="fas fa-sign-out-alt"></i> Sair
+                </a>
+            </div>
+      </li>
+      @endif
+    </ul>
+    </div>
     
     <!-- <form class="form-inline ml-3">
     <div class="input-group input-group-sm">
--- resources/views/web/login/_form.blade.php
+++ resources/views/web/login/_form.blade.php
@@ -1,8 +1,15 @@
-<div class="input-field row">
-        <label for="email">E-mail: </label>
-        <input type="text" name="email" id="email">
+<div class="card-body">
+    <div class="form-group row">
+        <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-10">
+            <input type="email" name="email" class="form-control" id="inputEmail3" placeholder="Email">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+        <div class="col-sm-10">
+            <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="Password">
+        </div>
+    </div>
 </div>
-<div class="input-field row">
-    <label for="password">Password: </label>
-    <input type="password" name="password" id="password">
-</div>
+<!-- /.card-body -->
