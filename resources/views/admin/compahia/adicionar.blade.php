6f09ed20e145bdad37e6d81a0383c02c48b7a2c6
henrique-temponi <tanzenheavely@gmail.com>
2020-10-07 10:39:42 -0300

    Melhora layout
    Coloca as paginas de relacionadas a compahia para a formatacao do adminlte

--- resources/views/admin/compahia/adicionar.blade.php
+++ resources/views/admin/compahia/adicionar.blade.php
@@ -2,16 +2,42 @@
 
 @section('conteudo')
 
-<h3>Adicionar Compahia</h3>
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0 text-dark">Adicionar</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">Painel Admin</a></li>
+          <li class="breadcrumb-item"><a href="{{ route('admin.compahias.listar') }}">lista de compahias</a></li>
+          <li class="breadcrumb-item active">Adicionar compahia</li>
+        </ol>
+      </div>
+    </div>
+  </div>
 
 @include('admin.errors')
 
-<form action="" method="post">
-
-    {{ csrf_field() }}
-    @include('admin.compahia._form')
-
-    <button type="submit" class="btn">Adicionar compahia</button>
-</form>
-
+<div class="content">
+  <div class="content-fluid">
+    <div class="col-8">
+      <div class="card">
+        <div class="card-header">
+          <h5 class="card-title">Adicionar compahia</h5>
+        </div>
+        <form action="{{ route('admin.compahias.adicionar') }}" method="post">
+          {{ csrf_field() }}
+          <div class="card-body">
+            @include('admin.compahia._form')
+          </div>
+          <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Adicionar compahia</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
