927129c83e57699e6b1eca037e6c7811e70b37ee
henrique-temponi <tanzenheavely@gmail.com>
2020-10-07 10:13:06 -0300

    Melhora layout
    Coloca as paginas de relacionadas ao destino para a formatacao do adminlte

--- resources/views/admin/destino/adicionar.blade.php
+++ resources/views/admin/destino/adicionar.blade.php
@@ -2,16 +2,44 @@
 
 @section('conteudo')
 
-<h3>Adicionar destino</h3>
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0 text-dark">Adicionar</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">Painel Admin</a></li>
+          <li class="breadcrumb-item"><a href="{{ route('admin.destinos.listar') }}">lista de destinos</a></li>
+          <li class="breadcrumb-item active">Adicionar Destino</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
 
 @include('admin.errors')
 
-<form action="" method="post">
-
-    {{ csrf_field() }}
-    @include('admin.destino._form')
-
-    <button type="submit" class="btn">Adicionar destino</button>
-</form>
+<div class="content">
+  <div class="content-fluid">
+    <div class="col-8">
+      <div class="card">
+        <div class="card-header">
+          <h5 class="card-title">Adicionar destino</h5>
+        </div>
+        <form action="{{ route('admin.destinos.adicionar') }}" method="post">
+          {{ csrf_field() }}
+          <div class="card-body">
+            @include('admin.destino._form')
+          </div>
+          <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Adicionar destino</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
--- resources/views/admin/destino/listar.blade.php
+++ resources/views/admin/destino/listar.blade.php
@@ -2,44 +2,60 @@
 
 @section('conteudo')
 
-<div class="row">
-
-    <h3>Destino disponiveis</h3>
-    <a class="btn" href="{{ route('admin.destinos.adicionar') }}">Adicionar Destino</a>
-
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0 text-dark">Destino</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">Painel Admin</a></li>
+          <li class="breadcrumb-item active">listagem de destino</li>
+        </ol>
+      </div>
+    </div>
+  </div>
 </div>
 
-
-<div class="divider"></div>
-
-<div class="row">
-    <table>
-        <thead>
-            <th>Nome</th>
-            <th>Abreviação</th>
-            <th>Quant. voos registrados</th>
-            <th>Opcões</th>
-
-        </thead>
-        <tbody>
-            @foreach($registro as $r)
-            <tr>
-                <td>{{ $r->nome }}</td>
-                <td>{{ $r->abreviacao }}</td>
-                <td>{{ $r->flight()->count()}}</td>
-                <td><a class="btn green" href="{{ route('admin.destinos.editar', $r->id) }}">Editar</a></td>
-                <td><a class="btn red" href="{{ route('admin.destinos.deletar', $r->id) }}">Deletar</a></td>
-                <!-- @if($r->flight()->count())
-                    <td><a class="btn green disabled" href="#">Editar</a></td>
-                    <td><a class="btn red disabled" href="#">Deletar</a></td>
-                @else -->
-                    <!-- <td><a class="btn green" href="{{ route('admin.destinos.editar', $r->id) }}">Editar</a></td>
-                    <td><a class="btn red" href="{{ route('admin.destinos.deletar', $r->id) }}">Deletar</a></td> -->
-                <!-- @endif -->
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+<div class="content">
+  <div class="content-fluid">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Destino registrados</h3>
+        </div>
+        <div class="card-body">
+          <div class="dataTables_wrapper dt-bootstrap4">
+            <div class="row">
+              <div class="col-sm-12">
+                <table class="table table-bordered table-striped dataTable">
+                  <thead>
+                    <th>Nome</th>
+                    <th>Abreviação</th>
+                    <th>Quant. voos registrados</th>
+                    <th>Opcões</th>
+                  </thead>
+                  <tbody>
+                    @foreach($registro as $r)
+                      <tr class="even">
+                        <td>{{ $r->nome }}</td>
+                        <td>{{ $r->abreviacao }}</td>
+                        <td>{{ $r->flight()->count()}}</td>
+                        <td>
+                            <a class="btn btn-warning" href="{{ route('admin.destinos.editar', $r->id) }}">Editar</a>
+                            <a class="btn btn-danger btn-space" href="{{ route('admin.destinos.deletar', $r->id) }}">Deletar</a>
+                        </td>
+                      </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-
 @endsection
