5a59b558224003067de44958c8ff029c4adf17a3
henrique-temponi <tanzenheavely@gmail.com>
2020-10-14 11:21:04 -0300

    Altera a autorizacao

--- resources/views/layout/_asideAdmin.blade.php
+++ resources/views/layout/_asideAdmin.blade.php
@@ -94,25 +94,6 @@
               <p>Usuario</p>
             </a>
           </li>
-          <li class="nav-header">LABELS</li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-danger"></i>
-              <p class="text">Important</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-warning"></i>
-              <p>Warning</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-info"></i>
-              <p>Informational</p>
-            </a>
-          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
